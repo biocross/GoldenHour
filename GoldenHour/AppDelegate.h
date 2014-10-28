6a8804fd9c48adf74c311bda7c0fe7ecd9030203
Siddharth Gupta <sids.1992@gmail.com>
2014-10-28 18:46:43 +0530

    GoldenHour

---
+++ GoldenHour/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoldenHour
+//
+//  Created by Siddharth on 28/10/14.
+//  Copyright (c) 2014 Siddharth Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoldenHour/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoldenHour
+//
+//  Created by Siddharth on 28/10/14.
+//  Copyright (c) 2014 Siddharth Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
